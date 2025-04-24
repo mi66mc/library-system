7653655ed07c884738d42feed1a510f9bd58ef23
mi66mc <miguelmaglarezi@gmail.com>
2025-04-24 17:42:40 -0300

    update: just a 'home page'

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,11 @@ app.use(express.json());
 
 app.use("api/books", booksRouter);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Books API!");
+});
+
+
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
