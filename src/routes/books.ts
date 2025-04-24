051a77009d280374070084260e933dc2aa3c8a4c
mi66mc <miguelmaglarezi@gmail.com>
2025-04-24 20:16:33 -0300

    update: route DELETE /api/books/:id added

--- src/routes/books.ts
+++ src/routes/books.ts
@@ -4,13 +4,13 @@ import pool from "../db";
 const router = express.Router();
 
 router.get("/", async (_, res) => {
-  try {
-    const { rows: books } = await pool.query("SELECT * FROM books");
-    res.json(books);
-  } catch (error) {
-    console.error("Error: ", error);
-    res.status(500).json({ error: "Error while trying to get books." });
-  }
+    try {
+        const { rows: books } = await pool.query("SELECT * FROM books");
+        res.json(books);
+    } catch (error) {
+        console.error("Error: ", error);
+        res.status(500).json({ error: "Error while trying to get books." });
+    }
 });
 
 router.post("/", async (req, res) => {
@@ -23,6 +23,25 @@ router.post("/", async (req, res) => {
     } catch (error) {
         console.error("Error: ", error);
         res.status(500).json({ error: "Error while trying to add a book." });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const query = "DELETE FROM books WHERE id = $1 RETURNING *";
+        const { rows: deletedBook } = await pool.query(query, [id]);
+        
+        if (deletedBook.length === 0) {
+            res.status(404).json({ error: "Book not found." });
+        } else {
+            res.json("Book deleted successfully.");
+        }
+        
+    } catch (error) {
+        console.error("Error: ", error);
+        res.status(500).json({ error: "Error while trying to delete a book." });
     }
 });
 
